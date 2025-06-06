3b06bb7dd16bfa7b75fd68ab8e30703dc199e44f
jodie-h <jodie.he@student.csd509j.net>
2025-06-06 11:39:34 -0700

    change tile map

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
 function switchLevel () {
-	
+    if (current_level == 1) {
+        tiles.setCurrentTilemap(tilemap`level1`)
+    }
+    createPlayer()
 }
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
 function createPlayer () {
     if (current_level == 1) {
-        for (let value of tiles.getTilesByType(assets.tile`myTile`)) {
+        for (let value of tiles.getTilesByType(assets.tile`myTile3`)) {
             tiles.placeOnTile(mySprite, value)
             tiles.setTileAt(value, assets.tile`transparency16`)
         }
@@ -11,6 +17,9 @@ function createPlayer () {
 }
 let mySprite: Sprite = null
 let current_level = 0
+scene.setBackgroundColor(12)
 current_level = 1
 mySprite = sprites.create(assets.image`even smaller`, SpriteKind.Player)
 controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
+switchLevel()
