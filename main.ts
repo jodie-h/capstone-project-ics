dfaf4efba3e99978f2c6e2ab421211c47a6d58a1
jodie-h <jodie.he@student.csd509j.net>
2025-06-06 11:07:27 -0700

    change tilemap and create player

--- main.ts
+++ main.ts
@@ -1,2 +1,16 @@
-let mySprite = sprites.create(assets.image`even smaller`, SpriteKind.Player)
+function switchLevel () {
+	
+}
+function createPlayer () {
+    if (current_level == 1) {
+        for (let value of tiles.getTilesByType(assets.tile`myTile`)) {
+            tiles.placeOnTile(mySprite, value)
+            tiles.setTileAt(value, assets.tile`transparency16`)
+        }
+    }
+}
+let mySprite: Sprite = null
+let current_level = 0
+current_level = 1
+mySprite = sprites.create(assets.image`even smaller`, SpriteKind.Player)
 controller.moveSprite(mySprite)
