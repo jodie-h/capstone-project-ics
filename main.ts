63a80ce66e87a4c8dcd68aa54e40e3fd2c12cc7a
jodie-h <jodie.he@student.csd509j.net>
2025-06-06 12:02:23 -0700

    start jump

--- main.ts
+++ main.ts
@@ -5,7 +5,9 @@ function switchLevel () {
     createPlayer()
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    if (mySprite.vy == 0) {
+        mySprite.vy = jump_speed
+    }
 })
 function createPlayer () {
     if (current_level == 1) {
@@ -13,8 +15,15 @@ function createPlayer () {
             tiles.placeOnTile(mySprite, value)
             tiles.setTileAt(value, assets.tile`transparency16`)
         }
+        mySprite.ay = gravity
     }
 }
+function setVariable () {
+    jump_speed = -150
+    gravity = 200
+}
+let gravity = 0
+let jump_speed = 0
 let mySprite: Sprite = null
 let current_level = 0
 scene.setBackgroundColor(12)
@@ -22,4 +31,5 @@ current_level = 1
 mySprite = sprites.create(assets.image`even smaller`, SpriteKind.Player)
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
+setVariable()
 switchLevel()
